3893e9c7e045a134710af8e64c72be305c15167d
VeriSkas <92_sveto4ka@mail.ru>
2022-11-16 20:48:13 +0300

    made other functions, error notification

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles/style.scss';
 import {
   btnsArr,
   scren,
-  numbersArr,
+  numbersAndSimpleOperatorsArr,
   mainOperatorsArr,
   additionalOperatorsArr,
   clearAllBtn,
@@ -14,6 +14,7 @@ let state = {
   secondOperand: '',
   operator: '',
   screenLength: 25,
+  roundingValue: 4,
 }
 
 scren.innerText = 0;
@@ -36,14 +37,62 @@ function clearState () {
     secondOperand: '',
     operator: '',
     screenLength: 25,
+    roundingValue: 4,
   };
 
   scren.innerText = 0;
 }
 
+function makeBtnType ( btnValue ) {
+  if ( btnValue === clearAllBtn ) { clearState() };
+  if ( numbersAndSimpleOperatorsArr.includes( btnValue ) && !state.operator ) { onChangeState( btnValue, 'firstOperand' ) };
+  if ( numbersAndSimpleOperatorsArr.includes( btnValue ) && state.operator ) { onChangeState( btnValue, 'secondOperand' ) };
+  if ( mainOperatorsArr.includes( btnValue ) && state.secondOperand) {
+    answerCalculateHandler();
+    state.secondOperand = '';
+    onChangeState( btnValue, 'operator' );
+  };
+  if ( mainOperatorsArr.includes( btnValue )) { onChangeState ( btnValue, 'operator' ) };
+  if ( btnValue === equal ) {
+    answerCalculateHandler();
+    state.operator = '';
+    innerTextHandler('firstOperand');
+  };
+  if ( additionalOperatorsArr.includes( btnValue )) {
+    state.operator = btnValue;
+    answerCalculateHandler();
+    state.operator = '';
+    innerTextHandler('firstOperand');
+  };
+}
+
+function onChangeState ( value, changedPlace ) {
+  const stringValue = String(value);
+
+  if ( state[ changedPlace ].length >= state.screenLength ) return;
+  if ( value === '.' && state[ changedPlace ].includes('.') ) return;
+
+  if ( value === '+/-' ) {
+    state[ changedPlace ] = String( 0 - state[ changedPlace ] );
+  } else if ( value === '%' ) {
+    state[ changedPlace ] = String( state[ changedPlace ] / 100 );
+  } else {
+    state[ changedPlace ] === '0' && value !== '.'
+      ? state[ changedPlace ] = stringValue
+      : state[ changedPlace ] += stringValue;
+  }
+
+  changedPlace === 'operator' && state.operator ? state.operator = stringValue : null;
+  innerTextHandler( changedPlace );
+}
+
+function innerTextHandler ( place ) {
+  scren.innerText = state[ place ];
+}
+
 function answerCalculateHandler () {
   const { firstOperand, secondOperand, operator } = state;
-console.log(state);
+
   switch ( operator ) {
     case '+':
       state.firstOperand = +firstOperand + +secondOperand;
@@ -55,54 +104,64 @@ console.log(state);
       state.firstOperand = +firstOperand * +secondOperand;
       break;
     case '÷':
-      state.firstOperand = +firstOperand / +secondOperand;
+      if (secondOperand === '0') { createErrorNotificationHandler('ERROR: You can`t divide by zero') };
+
+      secondOperand !== '0'
+        ? state.firstOperand = +firstOperand / +secondOperand
+        : state.firstOperand = '';
+      break;
+    case 'xᵞ':
+      state.firstOperand = ( +firstOperand ) ** ( +secondOperand );
+      break;
+    case 'ᵞ√x':
+      state.firstOperand = ( +firstOperand ) ** ( 1 / +secondOperand );
       break;
     case 'x²':
-      state.firstOperand = (+firstOperand) ** 2;
+      state.firstOperand = ( +firstOperand ) ** 2;
       break;
     case 'x³':
-      state.firstOperand = (+firstOperand) ** 3;
+      state.firstOperand = ( +firstOperand ) ** 3;
       break;
     case '²√x':
-      state.firstOperand = (+firstOperand) ** (1/2);
+      state.firstOperand = ( +firstOperand ) ** (1/2);
       break;
     case '³√x':
-      state.firstOperand = (+firstOperand) ** (1/3);
+      state.firstOperand = ( +firstOperand ) ** (1/3);
+      break;
+    case '10ᵡ':
+      state.firstOperand =  10 ** ( +firstOperand );
+      break;
+    case '1/x':
+      state.firstOperand =  1 / ( +firstOperand );
+      break;
+    case 'x!':
+      state.firstOperand =  factorial( +firstOperand );
       break;
   }
-}
 
-function makeBtnType (btnValue) {
-  if ( btnValue === clearAllBtn ) { clearState() };
-  if ( numbersArr.includes(btnValue) && !state.operator ) { onChangeState(btnValue, 'firstOperand') };
-  if ( numbersArr.includes(btnValue) && state.operator) { onChangeState(btnValue, 'secondOperand') };
-  if ( mainOperatorsArr.includes(btnValue) && state.secondOperand) {
-    answerCalculateHandler();
-    state.secondOperand = '';
-    onChangeState(btnValue, 'operator');
-  };
-  if ( mainOperatorsArr.includes(btnValue)) { onChangeState(btnValue, 'operator'); };
-  if ( btnValue === equal ) {
-    answerCalculateHandler();
-    state.operator = '';
-    innerTextHandler('firstOperand');
-  }
-  if ( additionalOperatorsArr.includes(btnValue) ) {
-    state.operator = btnValue;
-    answerCalculateHandler();
-    state.operator = '';
-    innerTextHandler('firstOperand');
+  if ( String( state.firstOperand ).includes('.')) {
+    state.firstOperand = ( +state.firstOperand ).toFixed(state.roundingValue);
   }
 }
 
-function innerTextHandler ( place ) {
-  scren.innerText = state[place];
+function factorial( num ) {
+  if ( num <= 1 ) {
+    return num;
+  }
+
+  return num * factorial( num - 1 );
 }
 
-function onChangeState ( value, changedPlace ) {
-  if ( state[changedPlace].length >= state.screenLength ) return;
+function createErrorNotificationHandler (errorMessage) {
+  const wrapper = document.querySelector('.wrapper');
+  const errorNotification = document.createElement('div');
 
-  state[changedPlace] += value;
-  changedPlace === 'operator' && state.operator ? state.operator = value : null;
-  innerTextHandler( changedPlace );
+  errorNotification.className ='error_notification';
+  errorNotification.innerText = errorMessage;
+  wrapper.append(errorNotification);
+
+  const timeout = setTimeout(() => {
+    errorNotification.className ='error_notification hide';
+    clearTimeout(timeout);
+  }, 5000)
 }
--- src/shared/variables.js
+++ src/shared/variables.js
@@ -1,7 +1,7 @@
 export const btnsArr = document.querySelectorAll('.item');
 export const scren = document.querySelector('.wrapper__content_calc_screen_text')
-export const numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-export const mainOperatorsArr = ['+', '-', '÷', '×', 'xᵞ', '%', 'x!', '1/x', '10ᵡ', 'ᵞ√x', 'mr', 'm-', 'm+', 'mc' ]
-export const additionalOperatorsArr = ['+/-', 'x²', 'x³', '³√x', '²√x'];
+export const numbersAndSimpleOperatorsArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+/-', '%'];
+export const mainOperatorsArr = ['+', '-', '÷', '×', 'xᵞ', 'ᵞ√x', 'mr', 'm-', 'm+', 'mc' ]
+export const additionalOperatorsArr = ['x²', 'x³', '³√x', '²√x', '10ᵡ', '1/x', 'x!'];
 export const clearAllBtn = 'AC';
 export const equal = '=';
