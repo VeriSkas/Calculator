a1dc8001a3cc9b159365149ed24e8daa1e2d89e0
VeriSkas <92_sveto4ka@mail.ru>
2022-11-24 12:15:22 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './styles/style.scss';
+import "./styles/style.scss";
 import {
   btnsArr,
   scren,
@@ -9,17 +9,17 @@ import {
   operatorsWithoutSecondOperand,
   memoryBtnsArr,
   stateKeys,
-} from './shared/variables';
-import { ERROR_MESSAGES } from './shared/errorMessages';
-import { createErrorNotificationHandler } from './components/errorNotification';
-import { factorial } from './shared/auxiliaryFormulas';
-import { INFO_MESSAGES } from './shared/infoMessages';
+} from "./shared/variables";
+import { ERROR_MESSAGES } from "./shared/errorMessages";
+import { createErrorNotificationHandler } from "./components/errorNotification";
+import { factorial } from "./shared/auxiliaryFormulas";
+import { INFO_MESSAGES } from "./shared/infoMessages";
 
 let state = {
-  firstOperand: '',
-  secondOperand: '',
-  operator: '',
-  memory: '',
+  firstOperand: "",
+  secondOperand: "",
+  operator: "",
+  memory: "",
   memoryTurnOn: false,
   screenLength: 25,
   roundingValue: 4,
@@ -28,10 +28,10 @@ let state = {
 scren.innerText = 0;
 
 function eventHandler() {
-  btnsArr.forEach(item => {
+  btnsArr.forEach((item) => {
     const btn = item;
 
-    btn.onclick = event => {
+    btn.onclick = (event) => {
       makeBtnType(event.target.innerText);
     };
   });
@@ -40,39 +40,49 @@ function eventHandler() {
 eventHandler();
 
 function clearState() {
-  state.firstOperand = '';
-  state.secondOperand = '';
-  state.operator = '';
+  state.firstOperand = "";
+  state.secondOperand = "";
+  state.operator = "";
   scren.innerText = 0;
 }
 
 function clearMemory() {
-  state.memory = '';
+  state.memory = "";
   state.memoryTurnOn = false;
   innerTextHandler(stateKeys.memoryTurnOn);
 }
 
 function makeBtnType(btnValue) {
-  if (btnValue === btnsValue.clearAllBtn) { clearState() }
-  if (btnValue === btnsValue.mcBtn) { clearMemory() }
-  if (btnValue === btnsValue.msBtn) { onChangeState(btnValue, stateKeys.memoryTurnOn) }
-  if ( memoryBtnsArr.includes(btnValue) ) { onChangeState(btnValue, stateKeys.memory) }
-  if ( numbersAndSimpleOperatorsArr.includes(btnValue) && !state.operator) {
-    onChangeState(btnValue, stateKeys.firstOperand)
+  if (btnValue === btnsValue.clearAllBtn) {
+    clearState();
   }
-  if ( numbersAndSimpleOperatorsArr.includes(btnValue) && state.operator) {
-    onChangeState(btnValue, stateKeys.secondOperand)
+  if (btnValue === btnsValue.mcBtn) {
+    clearMemory();
   }
-  if ( mainOperatorsArr.includes(btnValue) && state.secondOperand) {
+  if (btnValue === btnsValue.msBtn) {
+    onChangeState(btnValue, stateKeys.memoryTurnOn);
+  }
+  if (memoryBtnsArr.includes(btnValue)) {
+    onChangeState(btnValue, stateKeys.memory);
+  }
+  if (numbersAndSimpleOperatorsArr.includes(btnValue) && !state.operator) {
+    onChangeState(btnValue, stateKeys.firstOperand);
+  }
+  if (numbersAndSimpleOperatorsArr.includes(btnValue) && state.operator) {
+    onChangeState(btnValue, stateKeys.secondOperand);
+  }
+  if (mainOperatorsArr.includes(btnValue) && state.secondOperand) {
     answerCalculateHandler();
     onChangeState(btnValue, stateKeys.operator);
   }
-  if ( mainOperatorsArr.includes(btnValue) ) { onChangeState (btnValue, stateKeys.operator) }
+  if (mainOperatorsArr.includes(btnValue)) {
+    onChangeState(btnValue, stateKeys.operator);
+  }
   if (btnValue === btnsValue.equal) {
     answerCalculateHandler();
     innerTextHandler(stateKeys.firstOperand);
   }
-  if ( operatorsWithoutSecondOperand.includes(btnValue) ) {
+  if (operatorsWithoutSecondOperand.includes(btnValue)) {
     if (state.operator) {
       answerCalculateHandler();
     }
@@ -83,11 +93,12 @@ function makeBtnType(btnValue) {
   }
 }
 
-function onChangeState (value, changedPlace) {
+function onChangeState(value, changedPlace) {
   const stringValue = String(value);
 
   if (state[changedPlace].length >= state.screenLength) return;
-  if (value === btnsValue.dot && state[changedPlace].includes(btnsValue.dot) ) return;
+  if (value === btnsValue.dot && state[changedPlace].includes(btnsValue.dot))
+    return;
 
   if (value === btnsValue.plusMinus) {
     state[changedPlace] = String(0 - state[changedPlace]);
@@ -96,114 +107,122 @@ function onChangeState (value, changedPlace) {
   } else if (value === btnsValue.msBtn) {
     state[changedPlace] = true;
     state.secondOperand
-      ? state.memory = state.secondOperand
-      : state.memory = state.firstOperand;
+      ? (state.memory = state.secondOperand)
+      : (state.memory = state.firstOperand);
     clearState();
   } else if (value === btnsValue.mrBtn) {
     state.firstOperand = state[changedPlace];
   } else if (value === btnsValue.mPlus) {
     state[changedPlace] = +state[changedPlace] + +state.firstOperand;
-    state.firstOperand = '';
+    state.firstOperand = "";
   } else if (value === btnsValue.mMinus) {
     state[changedPlace] = state[changedPlace] - state.firstOperand;
-    state.firstOperand = '';
+    state.firstOperand = "";
   } else {
-    state[changedPlace] === btnsValue.zero && value !== '.'
-      ? state[changedPlace] = stringValue
-      : state[changedPlace] += stringValue;
+    state[changedPlace] === btnsValue.zero && value !== "."
+      ? (state[changedPlace] = stringValue)
+      : (state[changedPlace] += stringValue);
   }
 
-  changedPlace === stateKeys.operator && state.operator ? state.operator = stringValue : null;
+  changedPlace === stateKeys.operator && state.operator
+    ? (state.operator = stringValue)
+    : null;
   innerTextHandler(changedPlace);
 }
 
 function innerTextHandler(place) {
   if (place === stateKeys.memoryTurnOn) {
-    state[place] ? memory.innerText = INFO_MESSAGES.memoryText : memory.innerText = '';
-    state[place] ? scren.innerText = state.memory || 0 : scren.innerText = 0;
-    return
+    state[place]
+      ? (memory.innerText = INFO_MESSAGES.memoryText)
+      : (memory.innerText = "");
+    state[place]
+      ? (scren.innerText = state.memory || 0)
+      : (scren.innerText = 0);
+    return;
   }
-  scren.innerText = state[ place ] || 0;
+  scren.innerText = state[place] || 0;
 }
-console.log(       "dfvg");
+console.log("scazc ");
 
 function answerCalculateHandler() {
-  const {firstOperand, secondOperand, operator} = state;
+  const { firstOperand, secondOperand, operator } = state;
 
   switch (operator) {
     case btnsValue.plus:
       state.firstOperand = +firstOperand + +secondOperand;
-      state.secondOperand = '';
-      state.operator = '';
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.minus:
       state.firstOperand = +firstOperand - +secondOperand;
-      state.secondOperand = '';
-      state.operator = '';
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.multiple:
       state.firstOperand = +firstOperand * +secondOperand;
-      state.secondOperand = '';
-      state.operator = '';
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.divide:
-      if (secondOperand === btnsValue.zero) { createErrorNotificationHandler(ERROR_MESSAGES.divideByZero) }
+      if (secondOperand === btnsValue.zero) {
+        createErrorNotificationHandler(ERROR_MESSAGES.divideByZero);
+      }
 
       secondOperand !== btnsValue.zero
-        ? state.firstOperand = +firstOperand / +secondOperand
-        : state.firstOperand = '';
+        ? (state.firstOperand = +firstOperand / +secondOperand)
+        : (state.firstOperand = "");
 
-      state.secondOperand = '';
-      state.operator = '';
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.xInY:
-      state.firstOperand = (+firstOperand) ** (+secondOperand);
-      state.secondOperand = '';
-      state.operator = '';
+      state.firstOperand = (+firstOperand) ** +secondOperand;
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.yRootOfX:
       state.firstOperand = (+firstOperand) ** (1 / +secondOperand);
-      state.secondOperand = '';
-      state.operator = '';
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.square:
       state.firstOperand = (+firstOperand) ** 2;
-      state.secondOperand = '';
-      state.operator = '';
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.cube:
       state.firstOperand = (+firstOperand) ** 3;
-      state.secondOperand = '';
-      state.operator = '';
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.rootOfX:
       state.firstOperand = (+firstOperand) ** (1 / 2);
-      state.secondOperand = '';
-      state.operator = '';
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.cubeRootOfX:
       state.firstOperand = (+firstOperand) ** (1 / 3);
-      state.secondOperand = '';
-      state.operator = '';
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.tenInX:
-      state.firstOperand =  10 ** (+firstOperand);
-      state.secondOperand = '';
-      state.operator = '';
+      state.firstOperand = 10 ** +firstOperand;
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.oneDivideX:
-      state.firstOperand =  1 / (+firstOperand);
-      state.secondOperand = '';
-      state.operator = '';
+      state.firstOperand = 1 / +firstOperand;
+      state.secondOperand = "";
+      state.operator = "";
       break;
     case btnsValue.xFactorial:
-      state.firstOperand =  factorial(+firstOperand);
-      state.secondOperand = '';
-      state.operator = '';
+      state.firstOperand = factorial(+firstOperand);
+      state.secondOperand = "";
+      state.operator = "";
       break;
   }
 
-  if ( String(state.firstOperand).includes(btnsValue.dot) ) {
+  if (String(state.firstOperand).includes(btnsValue.dot)) {
     state.firstOperand = (+state.firstOperand).toFixed(state.roundingValue);
   }
 }
